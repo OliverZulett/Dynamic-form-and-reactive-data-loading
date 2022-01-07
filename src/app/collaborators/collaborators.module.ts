608f1169750f69e0f9fa182cecd0926d67df60a8
josh <oliverzulett@gmail.com>
2022-01-07 09:23:40 -0400

    collaborators module created

---
+++ src/app/collaborators/collaborators.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CollaboratorsComponent } from './components/collaborators/collaborators.component';
+
+@NgModule({
+  declarations: [
+    CollaboratorsComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CollaboratorsComponent
+  ]
+})
+export class CollaboratorsModule { }
---
+++ src/app/collaborators/components/collaborators/collaborators.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collaborators',
+  templateUrl: './collaborators.component.html',
+  styles: [
+  ]
+})
+export class CollaboratorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
