d4fac4b4b3e2f19eb98bd5f8a1a0b015434cc646
josh <oliverzulett@gmail.com>
2022-01-07 11:31:17 -0400

    dynamic form field

--- src/app/dynamicform/components/dynamic-form-field/dynamic-form-field.component.ts
+++ src/app/dynamicform/components/dynamic-form-field/dynamic-form-field.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormGroup, FormGroupDirective } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-form-field',
   templateUrl: './dynamic-form-field.component.html',
   styleUrls: ['./dynamic-form-field.component.scss']
 })
-export class DynamicFormFieldComponent implements OnInit {
+export class DynamicFormFieldComponent{
 
-  constructor() { }
+  @Input() id!: string;
+  @Input() title!: string;
+  @Input() type!: string;
+  @Input() value!: string;
+  
+  @Output() onBlurEmitted = new EventEmitter<string>();
+  
+  form!: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private rootFormGroup: FormGroupDirective) {
+    this.form = this.rootFormGroup.control;
+  }
+
+  onBlur() {
+    console.log('onBlur emitted from child component');
+    this.onBlurEmitted.emit(`onBlur emitted from item: ${this.id}`);
   }
 
 }
